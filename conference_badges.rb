30bed9d941a4892209d5b8ce29fbcdfec71c1502
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 23:27:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_creator
+    returns "Hello, my name is #{name}"
+end
+
+def batch_badge_creator
+
+
+end
+
+def assign_rooms(names)
+
+  return "Hello, #{names}! You'll be assigned to room #{number}!"
+end
+end
+
+def printer(name)
+
+end
